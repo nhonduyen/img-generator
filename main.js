826f7711c4c81048aa5cdb12135c8eaf062e4c77
nhonduyen <nhonduyen@gmail.com>
2017-11-08 16:30:46 +0700

    + Generate image with number and 3 random triangle with different size.

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
 var fs = require('fs');
-var imgGen = require('js-image-generator');
-var Jimp = require('jimp');
+var drawing = require('pngjs-draw');
+var png = drawing(require('pngjs').PNG);
+var async = require('async');
 
-// generate multiple images
-for (let i = 10 - 1; i >= 0; i--) {
-	imgGen.generateImage(500, 500, 80, function(err, image) {
-		console.log('generate image ' +i);
-		fs.writeFileSync('output/dummy' + i + '.png', image.data);
+var maxWidth = 500;
+var maxHeight = 500;
+
+for (let i = 100 - 1; i >= 0; i--) {
+	let num = i;
+	fs.createReadStream("input/blue.png")
+	.pipe(new png({ filterType: 4, width: maxWidth, height: maxHeight }))
+	.on('parsed', function() {
+		let arr = [0, 1, 2];
+		let pen = this;
+		async.each(arr, function(item, callback) {
+			let x = Math.round(Math.random()* (maxWidth/3)) + 10;
+			let y = Math.round(Math.random()* (maxWidth/3)) + 10;
+			let width = Math.round(Math.random() * 20) + 10;
+			pen.fillRect(x,y,width,width, pen.colors.red(100));
+			callback();
+			
+		}, function done() {
+
+			pen.drawText(5,5, num + "", pen.colors.green(100));
+			pen.pack().pipe(fs.createWriteStream('output/'+num+'.png'));
+			console.log('Write file '+ 'output/'+num+'.png')
+		});
+		
 	});
+
 }
-for (let i = 10 - 1; i >= 0; i--) {
-	let fileName = 'output/dummy'+ i +'.png';
-	let imageCaption = '' + i;
-	let loadedImage;
-	Jimp.read(fileName)
-    .then(function (image) {
-        loadedImage = image;
-        console.log('load image ' + fileName);
-        return Jimp.loadFont(Jimp.FONT_SANS_128_WHITE);
-    })
-    .then(function (font) {
-    	console.log('write file ' + fileName);
-        loadedImage.print(font, 20, 20, imageCaption)
-                   .write(fileName);
-    })
-    .catch(function (err) {
-        console.error(err);
-    });
-}
+
+
